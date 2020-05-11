34983acd9fa4203e71d18f1413a2785aadd65050
BalachandranKanagasundaram <bala.chandran@exelaonline.com>
2020-05-11 11:05:17 -0400

    Add Expense some functionality done

--- src/screens/ExpenseScreen.js
+++ src/screens/ExpenseScreen.js
@@ -1,5 +1,12 @@
 import React, { useState, useEffect } from "react";
-import { View, Text, ScrollView, FlatList, Modal, StyleSheet } from "react-native";
+import {
+  View,
+  Text,
+  ScrollView,
+  FlatList,
+  Modal,
+  StyleSheet,
+} from "react-native";
 import IsAuthenticated from "../utils";
 import Axios from "axios";
 import config from "../config";
@@ -7,7 +14,10 @@ import DataTableDetail from "../components/Datatable";
 import { Surface, Portal, Provider } from "react-native-paper";
 import Button from "../components/Button";
 import ToastIndicator from "../components/ToastIndicator";
-import { FAB } from 'react-native-paper';
+import { FAB, TextInput } from "react-native-paper";
+import Textbox from "../components/Textbox";
+import DropDown from "../components/DropDown";
+import CheckBoxDetail from "../components/CheckboxDetail";
 
 const ExpenseScreen = (props) => {
   const [SpentByUserData, setSpentByUserData] = useState([]);
@@ -16,12 +26,13 @@ const ExpenseScreen = (props) => {
   const [ExpenseData, setExpenseData] = useState([]);
   const [IsShow, setIsShow] = useState(false);
   const [errorMessage, setErrorMessage] = useState("");
+  const [IsAddExpense, setIsAddExpense] = useState(false);
 
   const [Id, setId] = useState("");
   const [ExpenseName, setExpenseName] = useState("");
   const [ExpenseDescription, setExpenseDescription] = useState("");
   const [IsDefaultExpense, setIsDefaultExpense] = useState(true);
-  const [Amount, setAmount] = useState("0");
+  const [Amount, setAmount] = useState("");
   const [SpentBy, setSpentBy] = useState("");
   const [SpentTo, setSpentTo] = useState([]);
 
@@ -56,6 +67,7 @@ const ExpenseScreen = (props) => {
         .then((res) => {
           if (res.data !== null || res.data !== undefined) {
             setExpenseData(res.data);
+            setIsAddExpense(false);
           }
         })
         .catch((err) => {
@@ -100,6 +112,7 @@ const ExpenseScreen = (props) => {
                 setErrorMessage("");
               }, 5000);
               setErrorMessage(res.data.message);
+              setIsAddExpense(false);
             } else {
               setIsShow(true);
               setTimeout(() => {
@@ -123,42 +136,115 @@ const ExpenseScreen = (props) => {
     }
   };
 
-  return (
-    <ScrollView style={{flex:1}}>
+  const checkedHandler = () => {
+    if (IsDefaultExpense) {
+      setIsDefaultExpense(false);
+    } else {
+      setIsDefaultExpense(true);
+    }
+  };
+
+  let wholeView = [];
+  if (!IsAddExpense) {
+    wholeView.push(
+      <View style={{ flex: 1 }}>
         <FAB
-    style={styles.fab}
-    large
-    icon="plus"
-    onPress={() => alert('Pressed')}
-  />
-      <ToastIndicator
-        isShowValue={IsShow}
-        position="top"
-        colorValue="error"
-        value={errorMessage}
-      ></ToastIndicator>
-      <View>
-        <DataTableDetail
-          headerData={["Name", "Amount", "", ""]}
-          innerData={ExpenseData}
-          userData={UserData}
-          deleteHandlerData={(obj) => {
-            deleteExpenseHandler(obj);
-          }}
+          style={styles.fab}
+          large
+          icon="plus"
+          onPress={() => alert("Pressed")}
         />
+        <ScrollView>
+          <ToastIndicator
+            isShowValue={IsShow}
+            position="top"
+            colorValue="error"
+            value={errorMessage}
+          ></ToastIndicator>
+          <View
+            style={{
+              marginTop: 60,
+              marginRight: 15,
+              marginBottom: 20,
+              marginLeft: 5,
+            }}
+          >
+            <DataTableDetail
+              headerData={["Name", "Amount", "", ""]}
+              innerData={ExpenseData}
+              userData={UserData}
+              deleteHandlerData={(obj) => {
+                deleteExpenseHandler(obj);
+              }}
+            />
+          </View>
+        </ScrollView>
       </View>
-    </ScrollView>
-  );
+    );
+  } else {
+    wholeView.push(
+      <View style={{ margin: 15 }}>
+        <Textbox
+          labeValue="Name"
+          textValue={ExpenseName}
+          onChangedTextHandler={(text) => {
+            setExpenseName(text);
+          }}
+          textStyle={{ backgroundColor: "white" }}
+        />
+        <Textbox
+          labeValue="Description"
+          textValue={ExpenseName}
+          onChangedTextHandler={(text) => {
+            setExpenseDescription(text);
+          }}
+          textStyle={{ backgroundColor: "white", marginTop: 15 }}
+        />
+        <TextInput
+          style={{ backgroundColor: "white", marginTop: 15 }}
+          mode="outlined"
+          render={() => (
+            <DropDown
+              dataValue={SpentByUserData}
+              selectedValue={SpentBy}
+              selectedStyle={{ marginTop: 3, marginLeft: 5 }}
+              valueChanged={(text) => {
+                setSpentBy(text);
+              }}
+              defaultValue="Select Spent By"
+            />
+          )}
+        />
+        <Textbox
+          labeValue="Amount"
+          textValue={Amount}
+          keyBoardTypeValue="numeric"
+          onChangedTextHandler={(text) => {
+            setAmount(text);
+          }}
+          textStyle={{ backgroundColor: "white", marginTop: 15 }}
+        />
+        <CheckBoxDetail
+          checkStyle={{ flex: 0.5 }}
+          IsSelected={IsDefaultExpense}
+          valueChanged={() => checkedHandler()}
+          labelValue="Is Default Expense"
+        ></CheckBoxDetail>
+      </View>
+    );
+  }
+
+  return <View>{ wholeView }</View>;
 };
 
 const styles = StyleSheet.create({
   fab: {
-    position: 'absolute',
-    right:0,
-    marginRight:15,
-    marginTop:-30,
-    zIndex:10
+    position: "absolute",
+    right: 0,
+    bottom:0,
+    marginRight: 15,
+    marginTop: 10,
   },
-})
+});
 
 export default ExpenseScreen;
